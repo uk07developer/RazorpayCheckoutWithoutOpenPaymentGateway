83d52f3509683daa67bb15ed9b85c3030c1b5aaf
Ashwani Kumar <106174772+uk07developer@users.noreply.github.com>
2024-07-03 14:33:48 +0530

    Update curl.php

--- curl.php
+++ curl.php
@@ -1,1 +1,10 @@
-A
+
+curl -u YOUR_KEY_ID:YOUR_KEY_SECRET \
+-X POST https://api.razorpay.com/v1/orders \
+-H "Content-Type: application/json" \
+-d '{
+    "amount": 50000,
+    "currency": "INR",
+    "receipt": "order_rcptid_11",
+    "payment_capture": 1
+}'
